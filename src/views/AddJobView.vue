d2551b85c72e96270a41e7c7f4dedb08e61bc42e
Saparmurat <saparmurat@protonmail.com>
2024-07-24 19:45:52 +0000

    add AddJobView

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,13 +1,53 @@
+<script setup>
+import axios from "axios";
+import router from '@/router';
+
+import { reactive, toRaw } from "vue";
+import { useToast } from "vue-toastification";
+
+const form = reactive({
+  type: "Full-Time",
+  title: "",
+  description: "",
+  salary: "$100K - $125K",
+  location: "",
+  company: {
+    name: "",
+    description: "",
+    contactEmail: "",
+    contactPhone: ""
+  }
+});
+
+const toast = useToast();
+
+const handleSubmit = async () => {
+  const formData = toRaw(form);
+
+  try {
+    const response = await axios.post("/api/jobs", formData);
+    toast.success('Job Added Successfully');
+    router.push(`/jobs/${response.data.id}`);
+  } catch (error) {
+    toast.error('Job Was Not Added');
+    console.error("Error adding job.");
+  }
+};
+
+
+
+</script>
+
 <template>
-  <section class="bg-green-50">
+  <section class="bg-gray-100">
     <div class="container m-auto max-w-2xl py-24">
       <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-        <form>
+        <form @submit.prevent="handleSubmit">
           <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
           <div class="mb-4">
             <label for="type" class="block text-gray-700 font-bold mb-2">Job Type</label>
-            <select id="type" name="type" class="border rounded w-full py-2 px-3" required>
+            <select v-model="form.type" id="type" name="type" class="border rounded w-full py-2 px-3" required>
               <option value="Full-Time">Full-Time</option>
               <option value="Part-Time">Part-Time</option>
               <option value="Remote">Remote</option>
@@ -17,18 +57,19 @@
 
           <div class="mb-4">
             <label class="block text-gray-700 font-bold mb-2">Job Listing Name</label>
-            <input type="text" id="name" name="name" class="border rounded w-full py-2 px-3 mb-2"
-              placeholder="eg. Beautiful Apartment In Miami" required />
+            <input v-model="form.title" type="text" id="name" name="name" class="border rounded w-full py-2 px-3 mb-2"
+              placeholder="eg. CEO for OpenAI In San Francisco, Bay Area" required />
           </div>
           <div class="mb-4">
             <label for="description" class="block text-gray-700 font-bold mb-2">Description</label>
-            <textarea id="description" name="description" class="border rounded w-full py-2 px-3" rows="4"
+            <textarea v-model="form.description" id="description" name="description"
+              class="border rounded w-full py-2 px-3" rows="4"
               placeholder="Add any job duties, expectations, requirements, etc"></textarea>
           </div>
 
           <div class="mb-4">
             <label for="type" class="block text-gray-700 font-bold mb-2">Salary</label>
-            <select id="salary" name="salary" class="border rounded w-full py-2 px-3" required>
+            <select v-model="form.salary" id="salary" name="salary" class="border rounded w-full py-2 px-3" required>
               <option value="Under $50K">under $50K</option>
               <option value="$50K - $60K">$50 - $60K</option>
               <option value="$60K - $70K">$60 - $70K</option>
@@ -47,33 +88,33 @@
             <label class="block text-gray-700 font-bold mb-2">
               Location
             </label>
-            <input type="text" id="location" name="location" class="border rounded w-full py-2 px-3 mb-2"
-              placeholder="Company Location" required />
+            <input v-model="form.location" type="text" id="location" name="location"
+              class="border rounded w-full py-2 px-3 mb-2" placeholder="Company Location" required />
           </div>
 
           <h3 class="text-2xl mb-5">Company Info</h3>
 
           <div class="mb-4">
             <label for="company" class="block text-gray-700 font-bold mb-2">Company Name</label>
-            <input type="text" id="company" name="company" class="border rounded w-full py-2 px-3"
-              placeholder="Company Name" />
+            <input v-model="form.company.name" type="text" id="company" name="company"
+              class="border rounded w-full py-2 px-3" placeholder="Company Name" />
           </div>
 
           <div class="mb-4">
             <label for="company_description" class="block text-gray-700 font-bold mb-2">Company Description</label>
-            <textarea id="company_description" name="company_description" class="border rounded w-full py-2 px-3"
-              rows="4" placeholder="What does your company do?"></textarea>
+            <textarea v-model="form.company.description" id="company_description" name="company_description"
+              class="border rounded w-full py-2 px-3" rows="4" placeholder="What does your company do?"></textarea>
           </div>
 
           <div class="mb-4">
             <label for="contact_email" class="block text-gray-700 font-bold mb-2">Contact Email</label>
-            <input type="email" id="contact_email" name="contact_email" class="border rounded w-full py-2 px-3"
-              placeholder="Email address for applicants" required />
+            <input v-model="form.company.contactEmail" type="email" id="contact_email" name="contact_email"
+              class="border rounded w-full py-2 px-3" placeholder="Email address for applicants" required />
           </div>
           <div class="mb-4">
             <label for="contact_phone" class="block text-gray-700 font-bold mb-2">Contact Phone</label>
-            <input type="tel" id="contact_phone" name="contact_phone" class="border rounded w-full py-2 px-3"
-              placeholder="Optional phone for applicants" />
+            <input v-model="form.company.contactPhone" type="tel" id="contact_phone" name="contact_phone"
+              class="border rounded w-full py-2 px-3" placeholder="Optional phone for applicants" />
           </div>
 
           <div>
