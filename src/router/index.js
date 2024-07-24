d134b13400a9a4f485d6e2c299e52ed56a02962e
Saparmurat <saparmurat@protonmail.com>
2024-07-24 12:24:10 +0000

    add AddJobView

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,13 @@ import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/", 
+      path: "/",
       name: "home",
       component: HomeView,
     },
@@ -17,6 +18,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/jobs/add",
+      name: "add-job",
+      component: AddJobView,
     },
     {
       path: "/jobs/:id",
